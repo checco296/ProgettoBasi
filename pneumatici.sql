838621ae7e17bad3b751a8be912518ee08433973
checco296 <francesco.ferraioli@studenti.unipd.it>
2022-11-03 00:13:06 +0100

    update

---
+++ pneumatici.sql
@@ -0,0 +1,18 @@
+INSERT INTO pneumatico(nome,nazione) VALUES
+('Pirelli','IT'),
+
+('Englebert', 'BE'),
+
+('Dunlop', 'UK'),
+
+('Firestone', 'US'),
+
+('Continental', 'DE'),
+
+('Avon', 'UK'),
+
+('Goodyear', 'US'),
+
+('Bridgestone', 'JP'),
+
+('Michelin', 'FR');
