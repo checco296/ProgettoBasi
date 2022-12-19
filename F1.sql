4d4627f8da65b12f2858c2e9ed0ebdbd5843b149
checco296 <francesco.ferraioli@studenti.unipd.it>
2022-12-19 18:48:22 +0100

    update

--- F1.sql
+++ F1.sql
@@ -1,4 +1,4 @@
-DROP TABLE  IF EXISTS circuito CASCADE;
+DROP TABLE IF EXISTS circuito CASCADE;
 DROP TABLE IF EXISTS squadra CASCADE;
 DROP TABLE IF EXISTS pilota CASCADE;
 DROP TABLE IF EXISTS motore CASCADE;
@@ -10,9 +10,9 @@ DROP TABLE IF EXISTS gara CASCADE;
 
 CREATE TABLE circuito(
     nome varchar(60),
-    alias varchar(60) NOT NULL,
+    alias varchar(60)  NOT NULL,
     nazione varchar(2) NOT NULL,
-    lunghezza int NOT NULL,
+    lunghezza int      NOT NULL,
     PRIMARY KEY(nome)
 );
 
@@ -25,11 +25,11 @@ CREATE TABLE squadra(
 
 CREATE TABLE pilota(
     codice_fiscale varchar(20),
-    nome varchar(20) NOT NULL,
-    cognome varchar(20) NOT NULL,
-    sesso varchar(1) NOT NULL,
-    nazionalita varchar(2) NOT NULL,
-    anno_nascita int NOT NULL,
+    nome varchar(20)         NOT NULL,
+    cognome varchar(20)      NOT NULL,
+    sesso varchar(1)         NOT NULL,
+    nazionalita varchar(2)   NOT NULL,
+    anno_nascita int         NOT NULL,
     anno_morte int,
     sigla_in_gara varchar(3) NOT NULL,
     PRIMARY KEY(codice_fiscale)
@@ -37,9 +37,9 @@ CREATE TABLE pilota(
 
 CREATE TABLE motore(
     nome varchar(40),
-    nazione varchar(2) NOT NULL,
-    cilindrata int NOT NULL,
-    tipologia VARCHAR(20),
+    nazione varchar(2)    NOT NULL,
+    cilindrata int        NOT NULL,
+    tipologia VARCHAR(20) NOT NULL,
     PRIMARY KEY(nome)
 );
 
@@ -51,13 +51,13 @@ CREATE TABLE pneumatico(
 
 CREATE TABLE autovettura(
     nome varchar(60),
-    squadra varchar(40) NOT NULL,
-    anno int NOT NULL,
-    motore varchar(40) NOT NULL,
+    squadra varchar(40)    NOT NULL,
+    anno int               NOT NULL,
+    motore varchar(40)     NOT NULL,
     pneumatico varchar(20) NOT NULL,
-    PRIMARY KEY(nome/*,squadra*/),
-    FOREIGN KEY(squadra)    REFERENCES squadra(nome) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY(motore)     REFERENCES motore(nome) ON UPDATE CASCADE ON DELETE CASCADE,
+    PRIMARY KEY(nome),
+    FOREIGN KEY(squadra)    REFERENCES squadra(nome)    ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY(motore)     REFERENCES motore(nome)     ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY(pneumatico) REFERENCES pneumatico(nome) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
@@ -67,20 +67,20 @@ CREATE TABLE partecipante(
     numero_in_gara int,
     PRIMARY KEY(codice_fiscale,vettura),
     FOREIGN KEY(codice_fiscale) REFERENCES pilota(codice_fiscale)  ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY(vettura)        REFERENCES autovettura(nome) ON UPDATE CASCADE ON DELETE CASCADE
+    FOREIGN KEY(vettura)        REFERENCES autovettura(nome)       ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE gara(
     anno int,
     gara_num int,
-    nome_gara varchar(60) NOT NULL,
+    nome_gara varchar(60)         NOT NULL,
     pilota_del_giorno varchar(20) NOT NULL,
-    pilota_veloce varchar(20) NOT NULL,
-    hot_lap time NOT NULL,
+    pilota_veloce varchar(20)     NOT NULL,
+    hot_lap time                  NOT NULL,
     PRIMARY KEY(anno,gara_num),
-    FOREIGN KEY(pilota_del_giorno) REFERENCES /*partecipante*/pilota(codice_fiscale) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY(pilota_veloce) REFERENCES /*partecipante*/pilota(codice_fiscale) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY(nome_gara) REFERENCES circuito(nome) ON UPDATE CASCADE ON DELETE CASCADE
+    FOREIGN KEY(pilota_del_giorno) REFERENCES pilota(codice_fiscale) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY(pilota_veloce)     REFERENCES pilota(codice_fiscale) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY(nome_gara)         REFERENCES circuito(nome)         ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE prestazione(
@@ -92,9 +92,9 @@ CREATE TABLE prestazione(
     tempo_q2 time,
     tempo_q3 time,
     posizione_arrivo int NOT NULL,
-    ritiro varchar(1) NOT NULL,
+    ritiro varchar(1)    NOT NULL,
     PRIMARY KEY(anno,gara_num,vettura,codice_fiscale),
-    FOREIGN KEY(anno,gara_num)              REFERENCES gara(anno,gara_num) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY(anno,gara_num)              REFERENCES gara(anno,gara_num)                  ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY(vettura,codice_fiscale)     REFERENCES partecipante(vettura,codice_fiscale) ON UPDATE CASCADE ON DELETE CASCADE,
 );
 
--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Progetto di basi di dati di Ferraioli Francesco,
 L'obbiettivo è creare un database funzionante nel quale effetture delle quary automatizzate mediante codice in C++.
  
  
+-Da riscrivere l'abstract una volta avuta la conferma che vada bene
