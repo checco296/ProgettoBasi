4a1db8f357788c171d1a4e3cbb37731d659a4f6c
checco296 <francesco.ferraioli@studenti.unipd.it>
2023-01-18 22:12:57 +0100

    update

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ Progetto di basi di dati di Ferraioli Francesco,
 L'obbiettivo è creare un database funzionante nel quale effetture delle quary automatizzate mediante codice in C++.
  
 -se vuoi fai la query sulle info dei piloti e una invece che data un pilota dice quate pole, vittorie e ritiri
--fare la relazione (iniziata,aggiungere foto output)
 
 -GROUP e aggregati 3 oki
 - devono comprendere più tabelle ok 
--- query_qualify_result.sql
+++ query_qualify_result.sql
@@ -2,7 +2,7 @@
 SELECT ROW_NUMBER() OVER (ORDER BY prestazione.tempo_q3,prestazione.tempo_q2,prestazione.tempo_q1) AS Posizione_Partenza,pilota.nome,pilota.cognome,pilota.sigla_in_gara,partecipante.numero_in_gara
 FROM pilota,partecipante,prestazione,autovettura
 WHERE prestazione.codice_fiscale = partecipante.codice_fiscale AND
-	prestazione.vettura = prestazione.vettura AND
+	partecipante.vettura = prestazione.vettura AND
 	partecipante.codice_fiscale = pilota.codice_fiscale AND
 	prestazione.anno = 2022 AND
 	prestazione.gara_num = 1 AND
